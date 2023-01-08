9cd4e17f32ae996523ab4050c844e0463163d9c8
Muslim shah <smuslimshah7@gmailcom>
2023-01-08 19:02:57 +0500

    adding items to cart and deleting items from  cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,51 +2,83 @@ const Product = require("../model/product");
 const Cart = require("../model/cart");
 
 //==>/index
-exports.getIndex = async(req, res, next) => {
+exports.getIndex = async (req, res, next) => {
   const data = await Product.findAll();
-  
-   res.render("shop/index", {
-     shopData: data,
-     pageTitle: "shop",
-   });
-  
-  
- 
+  res.render("shop/index", {
+    shopData: data,
+    pageTitle: "shop",
+  });
 };
 
 //==>/products
-exports.getProducts =async (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
   const data = await Product.findAll();
   res.render("shop/product-list", {
-      shopData: data,
-      pageTitle: "All proucts",
-    });
+    shopData: data,
+    pageTitle: "All proucts",
+  });
 };
 //==> cart
-exports.getCart = async(req, res, next) => {
-
+exports.getCart = async (req, res, next) => {
   const cart = await req.user.getCart();
   const cartProducts = await cart.getProducts();
-  console.log(cartProducts);
-  res.render("shop/cart", { pageTitle: "Cart", data: cartProducts })
+  res.render("shop/cart", { pageTitle: "Cart", data: cartProducts });
 };
 
 //==>/cart post
-exports.postCart = (req, res, next) => {
-  const product_id = req.body.product_id;
-  const productPrice = req.body.productPrice;
-  Cart.addProduct(product_id, productPrice);
-  // });
-  res.redirect("/cart");
+exports.postCart = async (req, res, next) => {
+  const product_id = req.body.product_id; //getting product id
+
+  try {
+    const cart = await req.user.getCart(); //getting user cart
+    const product = await cart.getProducts({ where: { id: product_id } }); //finding item in the cart
+
+    let cartProduct;
+    if (product.length > 0) {
+      //if item is in the cart
+      cartProduct = product;
+    }
+    let newQuantity = 1;
+    if (cartProduct) {
+      //item is in cart
+      console.log(
+        "already in cart================================================= "
+      );
+      let old_quantity = cartProduct[0].CartItem.quantity;
+      newQuantity = old_quantity + 1;
+    }
+    //if not already in cart it will be added to the cart
+
+    console.log(
+      " ================== new item added to the cart ================="
+    );
+
+    const newProduct = await Product.findByPk(product_id);
+    const added = await cart.addProduct(newProduct, {
+      through: {
+        quantity: newQuantity,
+      },
+    });
+
+    res.redirect("/cart");
+  } catch (err) {
+    console.log("item adding to cart error:"+err);
+  }
 };
 
 //=== delete cart item
-exports.postDeleteCart = (req, res, next) => {
+exports.postDeleteCart = async (req, res, next) => {
   const id = req.body.id;
-  Cart.deleteCartById(id);
-  res.redirect('/cart')
-
-}
+  try {
+    const cart = await req.user.getCart(); //getting cart
+    const product = await cart.getProducts({ where: { id: id } }); //finding item in cart
+    const prod_data = product[0];
+    prod_data.CartItem.destroy(); //deleting item from inBetween cartItems table
+    res.redirect("/cart");
+  } catch (err) {
+    console.log("deleting cart item error" + err);
+  }
+};
 
 //==>get orders
 exports.getOrders = (req, res, next) => {
@@ -60,14 +92,15 @@ exports.getCheckout = (req, res, next) => {
 
 //==>get product details /product/id
 exports.getProductDetails = (req, res, next) => {
-  const id = req.params.id;  
-  Product.getProductById(id).then(([d]) => {   
-    console.log(d[0]);
-    res.render("shop/product-details", { data: d[0] });
-  }).catch(err => {
-    if (err){
-      console.log(err);
-    }
-  })
- 
+  const id = req.params.id;
+  Product.getProductById(id)
+    .then(([d]) => {
+      console.log(d[0]);
+      res.render("shop/product-details", { data: d[0] });
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
 };
